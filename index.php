9f9ff305dd6ae1ce7613958e00f50ef5fb393545
Sinan Ceylan <Sinan@MacBook-Pro-van-Sinan.local>
2012-09-08 21:51:58 +0200

    Opzet opdracht 1 (basis)

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="style.css" type="text/css"/>
+        <title>Software Testen Opdracht 1</title>
+    </head>
+    <body>
+        <div><h1>Inleiding</h1>
+            <p>
+                
+            Groep 7 bestaat uit:
+            <ul>
+                <li>Twan Baten</li>
+                <li>Kevin Haring</li>
+                <li>Patrick Hollenberg</li>
+                <li>Sinan Ceylan</li>
+            </ul>
+            Op deze pagina vindt u drie verschillende scenario's waarin de eerste sin, 'SQL Injectie' toegelicht en gedemonsteerd zal worden.
+        </p>
+    </div>
+    <div>
+        <h2>Scenario 1:</h2>
+        <p>In het eerste scenario word er SQL Injectie toegepast op een inlogscherm.<br/><br/>
+        Gebruikersnaam:<input type="text" /><br/>
+        Wachtwoord:<input type="text" />
+        </p>
+    </div>
+    <div>
+        <h2>Scenario 2:</h2>
+    </div>
+    <div>
+        <h2>Scenario 3:</h2>
+    </div>
+</body>
+</html>
