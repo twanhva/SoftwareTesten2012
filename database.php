e5e0c1a7c44e18c7501db397d88b34fb04d62879
Twan <Twan@Informatica.lan>
2012-09-11 23:57:31 +0200

    database gegevens

--- database.php
+++ database.php
@@ -6,10 +6,10 @@ function connect() {
     $password = "5q4%0Jk0";
     $db_name = "sinanceyl";
 
-    $host = 'localhost';
-    $username = 'root';
-    $password = '';
-    $db_name = 'sinanceyl';
+//    $host = 'localhost';
+//    $username = 'root';
+//    $password = '';
+//    $db_name = 'sinanceyl';
     
     $conn = mysql_connect($host, $username, $password) or die("FOUT >> Er kan geen connectie gemaakt worden");
     mysql_select_db('sinanceyl') or die("FOUT >> De database kan niet worden gevonden");
