f1c4964e8a95939ec3de2104fd040e02a2402ff8
Twan Baten <twan.baten@hva.nl>
2012-09-27 21:04:34 +0200

    Opdracht 2 by Patrick

--- opdrachten/Opdracht_2.php
+++ opdrachten/Opdracht_2.php
@@ -1,3 +1,19 @@
+<?php
+if (isset($_POST['message'])) {
+    $message = $_POST['message'];
+    if (isset($_POST['onveilig'])) {
+        echo $message;
+    }
+
+    if (isset($_POST['veilig'])) {
+        if (preg_match('/^\w{5,25}$/', $message)) {
+            echo htmlentities($message);
+        } else {
+            echo "Dit bericht gaat niet getoont worden.";
+        }
+    }
+}
+?>
 <div id="header">
     <div id="logo">
         <h1><a href="#">Opdrachten</a></h1>
@@ -7,7 +23,10 @@
     <div id="content"><div class="box">
             <h2>Opdracht 2</h2>
             <p>
-                Hier komt opdracht 2 te staan..
+            <form method="POST">
+                <textarea name="message">Bericht hier.</textarea><br />
+                <input type="submit" name="veilig" value="Veilig!" /><input type="submit" name="onveilig" value="Onveilig!" />
+            </form>
             </p>
         </div>
     </div>
