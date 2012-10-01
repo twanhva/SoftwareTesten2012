760c1d5804ef4fea98d17e429f2622c6b5b9f285
Kevin Haring <K.KevinHaring@gmail.com>
2012-10-01 20:26:01 +0200

    -XSS beveiliging toegevoegd -Wat commentaar tussen de code gezet -Overbodig opdrachtpagina's verwijderd
    Nu alleen nog documentatie op de site bijvoegen...

--- opdrachten/Opdracht_2.php
+++ opdrachten/Opdracht_2.php
@@ -1,17 +1,14 @@
 <?php
 if (!empty($_POST['unsafe-submit'])) {
-    $username = $_POST['username'];
-    $message = $_POST['message'];
-    $query = "INSERT INTO `guestbook`(`username`, `message`) VALUES (:username, :message)";
-    $stmt = $dbh->prepare($query);
-    $stmt->bindParam('username', $username);
-    $stmt->bindParam('message', $message);
-    $stmt->execute();
-}
- 
-if (!empty($_POST['safe-submit'])) {
-    $username = $_POST['username'];
-    $message = $_POST['message'];
+    // Trim away whitespace
+    $username = trim($_POST['username']);
+    // If no input, then set username to Anonymous
+    $username = empty($username) ? "Anonymous" : $username;
+    
+    // Shorten message input to a maximum of 500 characters
+    $message = substr($_POST['message'], 0, 500);
+    
+    // Execute query to insert the data into the database
     $query = "INSERT INTO `guestbook`(`username`, `message`) VALUES (:username, :message)";
     $stmt = $dbh->prepare($query);
     $stmt->bindParam('username', $username);
@@ -19,7 +16,27 @@ if (!empty($_POST['safe-submit'])) {
     $stmt->execute();
 }
 
-$query = "SELECT * FROM (Select * FROM guestbook ORDER BY time DESC LIMIT 10) AS t";
+if (!empty($_POST['safe-submit'])) {
+    // Trim away whitespace
+    $username = trim($_POST['username']);
+    // Check for empty username and for usage of only alphabetical characters. If positive condition, 
+    // then set username to Anonymous
+    if(empty($username) || !preg_match("[a-zA-z]{2, 256}", $username)) {
+        $username = "Anonymous";
+    }
+    
+    // Shorten message input to a maximum of 500 characters
+    $message = substr($_POST['message'], 0, 500);
+    
+    // Execute query to insert the data into the database
+    $query = "INSERT INTO `guestbook`(`username`, `message`) VALUES (:username, :message)";
+    $stmt = $dbh->prepare($query);
+    $stmt->bindParam('username', $username);
+    $stmt->bindParam('message', $message);
+    $stmt->execute();
+}
+
+$query = "SELECT * FROM (Select * FROM guestbook ORDER BY time DESC LIMIT 5) AS t";
 $qResult = mysql_query($query);
 ?>
 
@@ -37,17 +54,32 @@ $qResult = mysql_query($query);
                 <?php
                 if (isset($qResult)) {
                     while ($res = mysql_fetch_array($qResult, MYSQL_ASSOC)) {
-                        ?>
-                        <div style="margin-bottom: 10px; width: 400px; min-height: 70px; border: 2px solid #333">
-                            <div style="width: 100%; min-height: 50px; border-bottom: 2px dotted #333;">
-                                <p style="line-height: 16px; margin: 5px; word-wrap: break-word; font-style: italic; font-size: 12px"><?php echo $res['message'] ?></p>
+                        // If the 'safe' form has been used to submit a message and the checkbox is unchecked, then protect
+                        // against Cross Site Scripting by using htmlentities.
+                        if (!empty($_POST['safe-submit']) && !isset($_POST['showCSS'])) {
+                            ?>
+                            <div style="margin-bottom: 10px; width: 400px; min-height: 70px; border: 2px solid #333">
+                                <div style="width: 100%; min-height: 50px; border-bottom: 2px dotted #333;">
+                                    <p style="line-height: 16px; margin: 5px; word-wrap: break-word; font-style: italic; font-size: 12px"><?php echo htmlentities($res['message']) ?></p>
+                                </div>
+                                <div style="height: 20px; width: 100%;">
+                                    <p style="text-align: center; font-style: italic; font-size: 10px">By <?php echo htmlentities($res['username']."(".$res['id'].")") ?> On <?php echo htmlentities($res['time']) ?></p>
+                                </div>
                             </div>
-                            <div style="height: 20px; width: 100%;">
-                                <p style="text-align: center; font-style: italic; font-size: 10px">By <?php echo $res['username'] ?> On <?php echo $res['time'] ?></p>
-                            </div>
-                        </div>
 
-                        <?php
+                            <?php
+                        } else {
+                            ?>
+                            <div style="margin-bottom: 10px; width: 400px; min-height: 70px; border: 2px solid #333">
+                                <div style="width: 100%; min-height: 50px; border-bottom: 2px dotted #333;">
+                                    <p style="line-height: 16px; margin: 5px; word-wrap: break-word; font-style: italic; font-size: 12px"><?php echo $res['message'] ?></p>
+                                </div>
+                                <div style="height: 20px; width: 100%;">
+                                    <p style="text-align: center; font-style: italic; font-size: 10px">By <?php echo $res['username']."(".$res['id'].")" ?> On <?php echo $res['time'] ?></p>
+                                </div>
+                            </div>
+                            <?php
+                        }
                     }
                 }
                 ?>
@@ -72,6 +104,7 @@ $qResult = mysql_query($query);
                         <p style="color:white; width: 100%">
                             Name: <input style="height: 20px; width: 150px" type="text" name="username" value="" /><br/>
                             Message: <br/><textarea style="resize: none; width: 100%; min-height: 100px" name="message"></textarea>
+                            <input type="checkbox" name="showCSS" value="showCSS" /> Show potential Cross Site Scripting<br/>
                             <input style="margin-top: 10px" name="safe-submit" type="submit" value="Submit"/>
                         </p>
                     </div>
