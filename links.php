ccd37c53831b61a6961d040b4f5d535ff9f003e9
Kevin Haring <K.KevinHaring@gmail.com>
2012-10-03 20:35:53 +0200

    Autumn Cleanup Added a link

--- links.php
+++ links.php
@@ -6,17 +6,13 @@
 <div id="page">
     <div id="content">
         <div class="box">
-            <p>Op deze pagina vermelden wij per opdracht de bronnen en links die wij erbij hebben gebruikt.
+            <p>Op deze pagina staan per opdracht de bronnen die zijn gebruikt bij het maken van de opdracht.
             </p>
             <h3>Opdracht 1</h3>
-            <li>Link 1</li>
-            <li>Link 2 enz..</li><br/>
+            <li><a href="www.wikipedia.org">Wikipedia</a></li>
             <h3>Opdracht 2</h3>
-            <h3>Opdracht 3</h3>
-            <h3>Opdracht 4</h3>
-            <h3>Opdracht 5</h3>
-            <h3>Opdracht 6</h3>
-            <h3>Opdracht 7</h3>
+            <li><a href="www.wikipedia.org">Wikipedia</a></li>
+            <li><a hrhref="https://www.owasp.org/index.php/Main_Page">OWASP</a>
         </div>
     </div>
     <br class="clearfix" />
